8b5e1f795ca4f3e9a3cc9ee6324df07d44d35434
phys3 <damjan.coric@mail.com>
2019-05-09 14:40:47 +0200

    Made router more readable

--- route-handlers/halls/index.js
+++ route-handlers/halls/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = new express.Router();
 const hallsController = require('./controller');
 
-router.route('/halls').get(hallsController.getHalls).post(hallsController.postHall);
-router.route('/halls/:hallid').get(hallsController.getSingleHall).delete(hallsController.deleteHall).put(hallsController.updateHall);
+router.route('/halls')
+    .get(hallsController.getHalls)
+    .post(hallsController.postHall);
+
+router.route('/halls/:hallid')
+    .get(hallsController.getSingleHall)
+    .delete(hallsController.deleteHall)
+    .put(hallsController.updateHall);
 
 module.exports = router;
