a4c74f4e1cd904f7939cb2eb0da0a0325df9921c
phys3 <damjan.coric@mail.com>
2019-05-09 13:50:18 +0200

    Controllers for users added

--- route-handlers/users/index.js
+++ route-handlers/users/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
 const usersController = require('./controller');
-router.route('/users').get(usersController.getUsers);
+router.route('/users').get(usersController.getUsers).post(usersController.postUser);
+router.route('/users/:userid').get(usersController.getSingleUser).delete(usersController.deleteUser).put(usersController.updateUser);
 module.exports = router;
